6d788656b87e491c449baf83503af062589f731f
abubaca4 <abubaca4@gmail.com>
2020-06-07 23:51:41 +0300

    client add player list

--- client.cpp
+++ client.cpp
@@ -69,11 +69,6 @@ void map_s(SDL_Renderer *renderer, const int w, const int h, std::vector<std::ve
         for (size_t j = 0; j < mat[i].size(); j++)
             switch (mat[i][j])
             {
-            case player_start_index: //отрисовка круга для игрока
-                SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-                DrawCircle(renderer, w_side_len * (j + 0.5), h_side_len * (i + 0.5), 0.8 * std::min(w / mat[i].size(), h / mat.size()) / 2);
-                break;
-
             case wall: //отрисовка чёрного прямоугольника для стены
             {
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
@@ -86,13 +81,28 @@ void map_s(SDL_Renderer *renderer, const int w, const int h, std::vector<std::ve
             }
 }
 
+void players_print(SDL_Renderer *renderer, const int w, const int h, const size_t x_size, const size_t y_size, std::vector<player> &player_list)
+{
+    int h_side_len = h / x_size; //высота одной ячейки
+    int w_side_len = w / y_size; //ширина одной ячейки
+    for (size_t i = 0; i < player_list.size(); i++)
+        if (player_list[i].is_alive)
+        {
+            SDL_SetRenderDrawColor(renderer, player_list[i].r, player_list[i].g, player_list[i].b, SDL_ALPHA_OPAQUE);
+            DrawCircle(renderer, w_side_len * (player_list[i].y + 0.5), h_side_len * (player_list[i].x + 0.5), 0.8 * std::min(w_side_len, h_side_len) / 2);
+        }
+}
+
 struct thread_data //структура для передачи информации в поток не объявленна в общем заголовочном файле так как может быть разная для сервера и клиента
 {
     pthread_mutex_t *time_mutex;
     struct timeval *update_time;
     pthread_mutex_t *map_mutex;
     std::vector<std::vector<field_cells_type>> *map_s;
+    pthread_mutex_t *player_mutex;
+    std::vector<player> *player_list;
     int sockfd;
+    size_t my_id;
 };
 
 void *reciver(void *data)
@@ -107,15 +117,29 @@ void *reciver(void *data)
         switch (input_format.type) //в зависимости от типа передаваемых данных
         {
         case field_type:
+            pthread_mutex_lock(prop.map_mutex);
             if ((*prop.map_s).size() != input_format.size || (*prop.map_s)[0].size() != input_format.second_size)
                 *prop.map_s = std::vector<std::vector<field_cells_type>>(input_format.size, std::vector<field_cells_type>(input_format.second_size));
-            pthread_mutex_lock(prop.map_mutex);
             for (size_t i = 0; i < (*prop.map_s).size(); i++) //получение поля построчно
                 n = recv(prop.sockfd, (field_cells_type *)(*prop.map_s)[i].data(), (*prop.map_s)[i].size() * sizeof(field_cells_type), 0);
             pthread_mutex_unlock(prop.map_mutex);
             pthread_mutex_lock(prop.time_mutex);
             gettimeofday(prop.update_time, NULL); //обновление метки времени
             pthread_mutex_unlock(prop.time_mutex);
+            break;
+        case player_list:
+            pthread_mutex_lock(prop.player_mutex);
+            if (prop.player_list->size() != input_format.size)
+                *prop.player_list = std::vector<player>(input_format.size);
+            n = recv(prop.sockfd, (player *)prop.player_list->data(), prop.player_list->size() * sizeof(player), 0);
+            pthread_mutex_unlock(prop.player_mutex);
+            pthread_mutex_lock(prop.time_mutex);
+            gettimeofday(prop.update_time, NULL); //обновление метки времени
+            pthread_mutex_unlock(prop.time_mutex);
+            break;
+
+        case my_number_from_list:
+            prop.my_id = input_format.size;
             break;
 
         default:
@@ -125,19 +149,6 @@ void *reciver(void *data)
     }
 
     return (void *)(0);
-}
-
-bool found_player(std::vector<std::vector<field_cells_type>> &mat, size_t &x, size_t &y) //поиск игрока на поле будет не актуален после введения списка игроков
-{
-    for (size_t i = 0; i < mat.size(); i++)
-        for (size_t j = 0; j < mat[i].size(); j++)
-            if (mat[i][j] == player_start_index)
-            {
-                y = i;
-                x = j;
-                return true;
-            }
-    return false;
 }
 
 int main(int argc, char *argv[])
@@ -167,14 +178,17 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    std::vector<player> player_list;                //список игроков
     std::vector<std::vector<field_cells_type>> mat; //матрица для хранения поля будет пересоздана при получении размера
-    struct timeval update_time, last_time;          //время обновления матрицы и время последней отрисованной марицы
+    struct timeval update_time, last_time;          //время обновления данных и время последних отрисованных данных
     gettimeofday(&update_time, NULL);               //занесение текущего времени
     last_time = update_time;                        //для избегания отрисовки до получения версии сервера
     pthread_mutex_t time_mutex;                     //мьютекс для времени
     pthread_mutex_init(&time_mutex, NULL);
     pthread_mutex_t map_mutex; //мьютекс для карты
     pthread_mutex_init(&map_mutex, NULL);
+    pthread_mutex_t player_mutex; //мьютекс для списка игроков
+    pthread_mutex_init(&player_mutex, NULL);
 
     struct sockaddr_in servaddr;
     int sockfd = socket(PF_INET, SOCK_STREAM, 0);
@@ -192,27 +206,30 @@ int main(int argc, char *argv[])
 
     int n = 1; // для хранения возврата соединения, если 0 соединение с сервером потеряно
 
-    size_t x = 0; //координаты игрока
-    size_t y = 0;
-
     thread_data share_data; //данные для передачи в поток
     share_data.sockfd = sockfd;
     share_data.map_s = &mat;
     share_data.map_mutex = &map_mutex;
     share_data.update_time = &update_time;
     share_data.time_mutex = &time_mutex;
+    share_data.player_mutex = &player_mutex;
+    share_data.player_list = &player_list;
 
     pthread_t reciver_thread; //запуск потока
     pthread_create(&reciver_thread, NULL, reciver, (void *)&share_data);
 
-    pthread_mutex_lock(&time_mutex);
-    while (!memcmp(&update_time, &last_time, sizeof(timeval))) //не даёт начать отрисовку пока не получена версия карты от сервера
+    pthread_mutex_lock(&map_mutex);
+    pthread_mutex_lock(&player_mutex);
+    while (mat.empty() || player_list.empty()) //не даёт начать отрисовку пока не получена карта и список игроков от сервера
     {
-        pthread_mutex_unlock(&time_mutex);
+        pthread_mutex_unlock(&map_mutex);
+        pthread_mutex_unlock(&player_mutex);
         usleep(1);
-        pthread_mutex_lock(&time_mutex);
+        pthread_mutex_lock(&map_mutex);
+        pthread_mutex_lock(&player_mutex);
     }
-    pthread_mutex_unlock(&time_mutex);
+    pthread_mutex_unlock(&map_mutex);
+    pthread_mutex_unlock(&player_mutex);
 
     if (SDL_Init(SDL_INIT_VIDEO) == 0)
     {
@@ -226,7 +243,7 @@ int main(int argc, char *argv[])
 
             while (!done && n) //пока не завершено или соединение не потеряно
             {
-                if (pthread_kill(reciver_thread, 0) != 0)
+                if (pthread_kill(reciver_thread, 0) != 0) //проверка жив ли поток(поток слушающий данные узнаёт о потере соединения первый и закрывается)
                 {
                     n = 0;
                     continue;
@@ -256,6 +273,10 @@ int main(int argc, char *argv[])
                     {
                         struct action_send temp; //отправка действий на сервер
                         temp.action = move;
+                        pthread_mutex_lock(&player_mutex);
+                        size_t x = player_list[share_data.my_id].x;
+                        size_t y = player_list[share_data.my_id].y;
+                        pthread_mutex_unlock(&player_mutex);
                         temp.from_x = x;
                         temp.from_y = y;
 
@@ -300,7 +321,6 @@ int main(int argc, char *argv[])
                 pthread_mutex_unlock(&time_mutex);
                 last_time = update_time; //обновить до отрисованной версии
                 need_review = SDL_FALSE; //опустить специальный флаг
-                found_player(mat, x, y);
 
                 SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
                 SDL_RenderClear(renderer); //очистить поле белым цветом
@@ -312,6 +332,9 @@ int main(int argc, char *argv[])
                 boards(renderer, w, h, mat.size(), mat[0].size());
                 map_s(renderer, w, h, mat);
                 pthread_mutex_unlock(&map_mutex);
+                pthread_mutex_lock(&player_mutex);
+                players_print(renderer, w, h, mat.size(), mat[0].size(), player_list);
+                pthread_mutex_unlock(&player_mutex);
 
                 SDL_RenderPresent(renderer);
             }
